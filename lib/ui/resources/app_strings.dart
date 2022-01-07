adb1869c3845743872ea8dbabc53e6af0e79b6f8
amr5aled <amrzezo10199@gmail.com>
2022-01-07 06:53:23 -0800

    second commit

--- lib/ui/resources/app_strings.dart
+++ lib/ui/resources/app_strings.dart
@@ -2,10 +2,14 @@ abstract class AppStrings {
   static const String required = "Required";
   static const String home = "Home";
   static const String viewMore = "View More";
-  static const String edit = "Edit";
+  static const String edit = "Edit Notes";
+  static const String add = "Add Notes";
   static const String remove = "Remove";
   static const String description = "Description";
   static const String status = "Status";
   static const String date = "Date";
   static const String title = "Title";
+  static const String time = "Time";
+  static const String save = "Save";
+  static const String update = "Update";
 }
